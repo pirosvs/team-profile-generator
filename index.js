194dfedf9d7c30936bc5b992cdc64eb803d9449e
darmstrong <armstrong2579@gmail.com>
2022-07-19 17:23:33 -0700

    add start/ ex prompt and write file, comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Require inquirer and fs so that we can ask questions in the terminal and create a file
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+// Creates our class Employee 
 class Employee {
     constructor(name, id, email) {
         this.name = name;
@@ -21,6 +23,24 @@ class Employee {
     }
 
     getRole() {
-        
+
     }
+}
+
+// List of questions asked by inquirer
+inquirer
+    .prompt([
+        {
+            type: "input"
+            name: "1"
+            message: "message"
+        }
+    ])
+
+// Writes file
+function writeToFile(data) {
+    const generateHTMLContent = generateHTML(data);
+    fs.writeFile(`${data.github.toLowerCase().split(' ').join('')}index.html`, generateHTMLContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created readme.md!')
+    );
 }
