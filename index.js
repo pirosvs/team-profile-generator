bacd58bd75c4337389934b4120d2796939557acf
darmstrong <armstrong2579@gmail.com>
2022-07-19 18:00:10 -0700

    add questions array/s

--- index.js
+++ index.js
@@ -27,12 +27,94 @@ class Employee {
     }
 }
 
+// Questions to use for inquirer -- perhaps consolidate the first 3 of each?
+// team manager’s name, employee ID, email address, and office number
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter manager name"
+    },
+
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter manager employee ID"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter manager email address"
+    },
+
+    {
+        type: "input",
+        name: "officeNum",
+        message: "Please enter manager office number"
+    }
+];
+
+// engineer’s name, ID, email, and GitHub username
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "engineerName",
+        message: "Please enter engineer name"
+    },
+
+    {
+        type: "input",
+        name: "engineerId",
+        message: "Please enter engineer employee ID"
+    },
+
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "Please enter engineer email address"
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter github username"
+    }
+];
+
+// intern’s name, ID, email, and school
+const internQuestions = [
+    {
+        type: "input",
+        name: "internName",
+        message: "Please enter intern name"
+    },
+
+    {
+        type: "input",
+        name: "internId",
+        message: "Please enter intern employee ID"
+    },
+
+    {
+        type: "input",
+        name: "internEmail",
+        message: "Please enter intern email address"
+    },
+
+    {
+        type: "input",
+        name: "schoolName",
+        message: "Please enter intern school name"
+    }
+];
+
 // List of questions asked by inquirer
 inquirer
     .prompt([
         {
-            type: "input"
-            name: "1"
+            type: "input",
+            name: "1",
             message: "message"
         }
     ])
