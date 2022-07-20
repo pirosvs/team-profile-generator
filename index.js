54afc8b84883a8cfd10750c9356f0c442b34f215
darmstrong <armstrong2579@gmail.com>
2022-07-19 17:07:25 -0700

    add employee class structure and requires

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+
+    }
+
+    getId() {
+
+    }
+
+    getEmail() {
+
+    }
+
+    getRole() {
+        
+    }
+}
