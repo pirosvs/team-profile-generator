942d035f6441a9c8e9eda9fa734d76bfb09ca879
darmstrong <armstrong2579@gmail.com>
2022-07-20 18:12:43 -0700

    add manager tests

---
+++ tests/manager.test.js
@@ -0,0 +1,23 @@
+const Employee = require('./lib/employee.js');
+const Manager = require('./lib/manager.js');
+
+describe("Manager", () => {
+    // Test for all new case github when initializing a new Engineer object
+    describe("Initialization", () => {
+      it("should create an object with a name, id, email, and role if provided valid arguments", () => {
+        const manager = new Manager("Damien", 345, "damiensmail@mail.com", 678);
+  
+        // Verify that the new object has the correct properties
+        // expect(manager.name).toEqual("Damien");
+        // expect(manager.id).toEqual(345);
+        // expect(manager.email).toEqual("damiensmail@mail.com");
+        expect(manager.officeNumber).toEqual(678);
+      });
+
+      it("should throw an error if not provided an office number", () => {
+        const cb = () => new Employee("Damien", 345, "damiensmail@mail.com");
+        const err = new Error("Expected parameter 'officeNumber' not found");
+        expect(cb).toThrowError(err);
+      });
+    })
+})
