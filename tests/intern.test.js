b341ff7948e471a8997fa273b3fc1d23c4be3346
darmstrong <armstrong2579@gmail.com>
2022-07-20 18:19:19 -0700

    add intern tests

---
+++ tests/intern.test.js
@@ -0,0 +1,29 @@
+const Employee = require('./lib/employee.js');
+const Intern = require('./lib/intern.js');
+
+describe("Intern", () => {
+    // Test for our new case school when initializing a new Intern object
+    describe("Initialization", () => {
+      it("should create an object with a name, id, email, and school if provided valid arguments", () => {
+        const intern = new Intern("Damien", 345, "damiensmail@mail.com", 678);
+  
+        // Verify that the new object has the correct properties
+        // expect(intern.name).toEqual("Damien");
+        // expect(intern.id).toEqual(345);
+        // expect(intern.email).toEqual("damiensmail@mail.com");
+        expect(intern.school).toEqual("Mills College");
+      });
+
+      it("should throw an error if not provided an school", () => {
+        const cb = () => new Employee("Damien", 345, "damiensmail@mail.com");
+        const err = new Error("Expected parameter 'school' to be a non-empty string`");
+        expect(cb).toThrowError(err);
+      });
+
+      it("should throw an error if 'school' is not a string", () => {
+        const cb = () => new Employee("Damien", 345, "damiensmail@mail.com", 45);
+        const err = new Error("Expected parameter 'school' to be a non-empty string");
+        expect(cb).toThrowError(err);
+      });
+    })
+})
